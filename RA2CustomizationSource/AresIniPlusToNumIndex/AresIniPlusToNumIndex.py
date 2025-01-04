89864984dd2162eba2163482662c511fd3fdde06
PC-DOS <suicunexp@outlook.com>
2025-01-05 01:36:07 +0800

    Create AresIniPlusToNumIndex.py

---
+++ RA2CustomizationSource/AresIniPlusToNumIndex/AresIniPlusToNumIndex.py
@@ -0,0 +1,63 @@
+import array
+
+def IsStringNullOrEmpty(sStringToTest : str) -> bool :
+    if sStringToTest is None :
+        return True
+    #End If
+
+    if len(sStringToTest) == 0 :
+        return True
+    #End If
+
+    if sStringToTest.isspace() :
+        return True
+    #End If
+
+    return False
+#End Function
+
+def SplitString(sStringToSplit : str, sSeparator : str = None, nMaxSplitCount : int = -1, RemoveEmptyEntries : bool = False) -> array :
+    # Split string
+    arrResult = sStringToSplit.split(sep=sSeparator, maxsplit=nMaxSplitCount)
+
+    # Remove empty enrties
+    if RemoveEmptyEntries :
+        arrResult = list(filter(None, arrResult))
+    #End If
+
+    return arrResult
+#End Function
+
+if __name__ == "__main__" :
+    # Convert Ares's "+=" to numbered indexes
+    arrConvertResults = []
+
+    iInitialIndex = int(input("Please input initial index:"))
+    iCurrentIndex = iInitialIndex
+
+    while True :
+        sIniLine = input()
+        if sIniLine == ";;END;;" :
+            break
+        #End If
+
+        arrIniParam = SplitString(sIniLine, "=", 1, False)
+
+        if len(arrIniParam) >= 2 :
+            if arrIniParam[0].strip() == "+" :
+                arrIniParam[0] = str(iCurrentIndex)
+            #End If
+            arrConvertResults.append(f"{arrIniParam[0]}={arrIniParam[1]}")
+            iCurrentIndex = iCurrentIndex + 1
+        elif len(arrIniParam) == 1 :
+            arrConvertResults.append(arrIniParam[0])
+        else :
+            arrConvertResults.append("")
+        #End If
+    #End While
+
+    print("Converted indexes:")
+    for IniLine in arrConvertResults :
+        print(IniLine)
+    #Next
+#End If
