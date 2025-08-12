1674be877f2a34a20d8ae76f57c260a114d54632
PC-DOS <suicunexp@outlook.com>
2025-08-12 15:48:29 +0800

    Update ~~~SCP-EQ-Tools_defines_override.lua

--- HoI4/SCP-EQ-Tools/SCP-EQ-Tools/common/defines/~~~SCP-EQ-Tools_defines_override.lua
+++ HoI4/SCP-EQ-Tools/SCP-EQ-Tools/common/defines/~~~SCP-EQ-Tools_defines_override.lua
@@ -2,8 +2,8 @@
 -- Please DO NOT remove the "~" character in file name, this ensures that this file will be loaded at last, overriding previous definitions
 
 -- Opinion value modifier
--- TRUST_VALUE controls the minimal and maximal value of a single opinion modifier
--- OPINION_VALUE controls the minimal and maximal value of the sum of all opinion modifiers
+-- MIN_TRUST_VALUE and MAX_TRUST_VALUE controls the minimal and maximal value of a single opinion modifier
+-- MIN_OPINION_VALUE and MAX_OPINION_VALUE controls the minimal and maximal value of the sum of all opinion modifiers
 -- Setting them to values over 10000 may cause overflow 
 NDefines.NDiplomacy.MAX_TRUST_VALUE = 5245
 NDefines.NDiplomacy.MIN_TRUST_VALUE = -5245
