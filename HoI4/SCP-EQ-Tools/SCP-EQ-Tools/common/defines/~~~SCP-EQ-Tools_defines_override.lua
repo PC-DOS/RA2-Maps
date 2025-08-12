4f3e2aa79d5778dc9ebaf60174560c1625b5fd5d
PC-DOS <suicunexp@outlook.com>
2025-08-12 15:47:47 +0800

    Diplomacy fix

--- HoI4/SCP-EQ-Tools/SCP-EQ-Tools/common/defines/~~~SCP-EQ-Tools_defines_override.lua
+++ HoI4/SCP-EQ-Tools/SCP-EQ-Tools/common/defines/~~~SCP-EQ-Tools_defines_override.lua
@@ -2,8 +2,11 @@
 -- Please DO NOT remove the "~" character in file name, this ensures that this file will be loaded at last, overriding previous definitions
 
 -- Opinion value modifier
-NDefines.NDiplomacy.MAX_OPINION_VALUE = 245245
-NDefines.NDiplomacy.MIN_OPINION_VALUE = -245245
+-- TRUST_VALUE controls the minimal and maximal value of a single opinion modifier
+-- OPINION_VALUE controls the minimal and maximal value of the sum of all opinion modifiers
+-- Setting them to values over 10000 may cause overflow 
+NDefines.NDiplomacy.MAX_TRUST_VALUE = 5245
+NDefines.NDiplomacy.MIN_TRUST_VALUE = -5245
 -- Faster volunteer sending
 NDefines.NDiplomacy.VOLUNTEERS_TRANSFER_SPEED = 1
 -- Focus boosting
