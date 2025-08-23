712712e32416f3eb82aaa2115ae128c3394f742f
PC-DOS <suicunexp@outlook.com>
2025-08-23 21:30:16 +0800

    Update intelligence agency upgrade

--- HoI4/SCP-EQ-Tools/SCP-EQ-Tools/_SCP-EQ-Tools_out_game_tools/GenerateNationalFocusTokens/GenerateNationalFocusTokens.py
+++ HoI4/SCP-EQ-Tools/SCP-EQ-Tools/_SCP-EQ-Tools_out_game_tools/GenerateNationalFocusTokens/GenerateNationalFocusTokens.py
@@ -1,4 +1,4 @@
-# Generate tokens of base HoI4 game and all installed mods
+# Generate tokens of national focuses from base HoI4 game and all installed mods
 # Required for "Complete current focus" cheating
 
 # Public libraries
